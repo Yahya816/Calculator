4bfd114108df3a223cf237851ad147258c2bcc48
Yahya816 <107574070+Yahya816@users.noreply.github.com>
2022-09-23 21:09:51 +0530

    Update calculator1.cxx

--- calculator1.cxx
+++ calculator1.cxx
@@ -1,34 +1,54 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+int main()
+{
+	char oprator;
+	double first, second;
+	do
+	{
+		printf("\n\n\tuse oprator (+ , - , * , /) \n\n");
+		printf("type like this a+b : ");
 
-  char oprator;
-  double first, second;
-  
-  printf("\tuse oprator (+),(-),(*),(/) \n\n");
-  printf("type like this 1+2 : ");
-  scanf("%lf%c%lf",&first ,&oprator,&second);
-  printf("\n THE ASWER IS  : ");
+		scanf("%lf%c%lf", &first, &oprator, &second);
 
-  
-  switch (oprator) {
-    case '+':
-      printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
-      break;
-    case '-':
-      printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
-      break;
-    case '*':
-      printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
-      break;
-    case '/':
-      printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
-      break;
-    
-    default:
-      printf("Error! operator is not correct");
-  }
-  printf("\n\n\t\t\t\t     - made by YAHYA\n\n");
+		printf("\n THE ASWER IS  : ");
 
-  return 0;
+		switch (oprator)
+		{
+		case '+':
+			printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+			break;
+		case '-':
+			printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+			break;
+		case '*':
+			printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+			break;
+		case '/':
+			printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+			break;
+
+		default:
+			printf("Error! operator is not correct❌\n\n\tType question like this (a+b) ");
+		}
+		
+		printf("\n\n\n\t\t\t\t       - made by YAHYA\n\n");
+
+
+		char jp[20];
+		scanf("%s", &jp);
+
+		if (strcmp(jp, "exit") == 0){
+			printf("\n\n\n\n \t\t\t - EXIT -");
+			break;
+		}
+		else{
+			continue;
+		}
+
+	} while (true);
+
+	return 0;
+	
 }
